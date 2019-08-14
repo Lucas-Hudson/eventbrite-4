fcda3676ea80f1453dfa221e91d421c5d3b36682
Lucas-Hudson <lucashudson.contact@gmail.com>
2019-08-14 11:18:11 +0200

    create event and sending email

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,4 +8,16 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
+  def create
+
+    @user = current_user
+
+    @event = Event.new(:administrator => @user, :start_date => params[:start_date], :duration => params[:duration], :title => params[:title], :description => params[:description], :price => params[:price], :location => params[:location])
+      if @event.save
+        redirect_to root_path
+      else
+        render events_path
+      end
+  end
+
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,7 +3,7 @@ class Attendance < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  #after_create :attending_event_send
+  after_create :attending_event_send
 
   def attending_event_send
     UserMailer.attending_event_email(self.event.administrator, self.user).deliver_now
